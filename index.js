d635f850a38f74b7a17cc0eb2145835a667ccdfb
TenZ123321 <jespinlim@gmail.com>
2026-03-29 12:34:24 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const pusher = new Pusher('4ab0a5b2186b6c48de02', {
+    cluster: 'ap1'
+});
+const channel = pusher.subscribe('my-channel');
+const wadah = document.getElementById('kotak-chat');
+channel.bind('my-event', function(data) {
+    const namaSaya = document.getElementById('nama-user').value;
+    const tipe = (data.nama === namaSaya) ? 'me' : 'others';
+    
+    wadah.innerHTML += `<div class="bubble ${tipe}"><b>${data.nama}:</b> ${data.pesan}</div>`;
+    wadah.scrollTop = wadah.scrollHeight;
+});
+document.getElementById('tombol-kirim').onclick = function() {
+    const nama = document.getElementById('nama-user').value;
+    const pesan = document.getElementById('isi-pesan').value;
+
+    if (nama && pesan) {
+        fetch('/api/send-message', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ nama: nama, pesan: pesan })
+        });
+
+        // Cukup kosongin input pesan aja
+        document.getElementById('isi-pesan').value = "";
+    }
+    };  
+document.getElementById('isi-pesan').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        document.getElementById('tombol-kirim').click();
+    }
+});
